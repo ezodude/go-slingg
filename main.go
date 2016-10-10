112c91dd275f8a96c1f0f7c09331307c0766f052
Ezo Saleh <ezo@the-experimenters.com>
2016-10-10 11:33:40 +0100

    Added basic CLI functionality.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"errors"
+	"flag"
 	"fmt"
 	"github.com/ezodude/go-slingg/batching"
 	"github.com/ezodude/go-slingg/xlsx"
@@ -17,20 +19,47 @@ func doError(err error) {
 	}
 }
 
-func postWorker(data []string) {
+func postWorker(url string, data []string) {
 	for _, json := range data {
 		payload := bytes.NewBufferString(json)
-		res, _ := http.Post("https://httpbin.org/post", "application/json; charset=utf-8", payload)
+		res, _ := http.Post(url, "application/json; charset=utf-8", payload)
 		io.Copy(os.Stdout, res.Body)
 	}
 }
 
 func main() {
-	excelFileName := "sample.xlsx"
+	var excelFileName string
+
+	targetUrlPtr := flag.String("url", "", "URL to http POST to.")
+	flag.Usage = func() {
+		fmt.Printf("\n  Usage: %s [flags] xlsx_file\n", os.Args[0])
+		fmt.Printf("\n  Slingg translates rows in an .XLSX file to JSON POST requests.\n")
+		fmt.Printf("\n  Options:\n\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	for index, arg := range flag.Args() {
+		if index == 0 {
+			excelFileName = arg
+		}
+	}
+
+	if len(*targetUrlPtr) == 0 {
+		doError(errors.New("No url specified!"))
+	}
+
+	if len(excelFileName) == 0 {
+		doError(errors.New("No excel filename specified!"))
+	}
+
 	err := xlsx.Load(excelFileName)
 	doError(err)
 
 	data, err := xlsx.Json()
 	doError(err)
-	batching.Batcher(data, 10, postWorker)
+	batching.Batcher(data, 10, func(data []string) {
+		postWorker(*targetUrlPtr, data)
+	})
 }
